c96ee48698533e21c0b9eeca55071ed19ba57678
Sam Noedel <sam.noedel@gmail.com>
2016-11-19 13:49:23 -0800

    Add auto-config loading

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 'use strict';
 
-const restify = require('restify');
+const restify = require('restify'),
+  Piloted = require('piloted'),
+  ContainerPilot = require('/etc/containerpilot.json');
+
+Piloted.config(ContainerPilot, err => console.error(err));
 
 let server = restify.createServer();
-
 server.get('/status', require('./handlers/status'));
 
 server.listen(8080, () => {
